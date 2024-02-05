555d8944f7aa64f105991980f1d108ea323f59b2
Muhammad Zeeshan <148358668+MuhammadZeeshanx@users.noreply.github.com>
2024-02-05 01:40:29 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Age Checker App in Python
+
+age = int(input("Enter Your age" + " :"))
+
+if age <=10 :
+    print("Your Are small")
+elif age <=20 :
+    print("You are Teen Boy")
+elif age <=60:
+    print("Your age Younger")
+elif age<=70:
+    print("You are over age")
+else :
+    print("Need Help")
